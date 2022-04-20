85626006c52f8749af573a63a809edfa5b5eba83
Agil Bashirov <agil.bashirov@outlook.com>
2022-04-20 12:05:32 +0400

    Services is ready

---
+++ Lunatech.Application/Model/Dto/Category/GetCategoryDetailDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lunatech.Application.Model.Dto.Category
+{
+    public class GetCategoryDetailDto
+    {
+        public string Name { get; set; }
+    }
+}

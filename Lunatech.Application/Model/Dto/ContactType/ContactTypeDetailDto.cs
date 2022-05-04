6e923306dba0992fbb64c09e1e400d7bfa2dd1de
eltun.ismayilov <eltun.ismayilov@ELTUN-I>
2022-05-04 15:51:18 +0400

    ContactType-CRUD :)

---
+++ Lunatech.Application/Model/Dto/ContactType/ContactTypeDetailDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Lunatech.Application.Model.Dto.ContactType
+{
+    public class ContactTypeDetailDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int LangId { get; set; }
+    }
+}
